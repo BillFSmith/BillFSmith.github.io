1a6d88b1af26ece5cbbe0a796cde2feee52d7101
BillFSmith <william.f.smith89@gmail.com>
2024-11-05 12:26:49 +0000

    first image

--- sound_player_script.js
+++ sound_player_script.js
@@ -1,35 +1,34 @@
-
-
 const buttonRand = document.querySelector('#buttonRand');
-const buttonDelete = document.querySelector('#buttonDelete');
-const textList = document.querySelector("#textbox");
-const animToggle = document.querySelector("#animations-toggle");
-
-
 const playSound = document.querySelector("#playSound");
-
-
+const quantity = document.querySelector("#quantity");
 
 buttonRand.onclick = playFun;
-buttonDelete.onclick = playFun;
-animToggle.onclick = playFun;
 
-let delIncr = 6000;
+// let delIncr = 6000;
 
 function playAudio() {
   playSound.play();
 }
 
-function printIt(text){
-	textList.value = text
+function delButtonText() {
+  buttonRand.innerText = "finished"
 }
 
 function playFun() {	
-	for (let i=0;i<100;i++) {
+
+	delayInMs = Number(quantity.value) * 1000 * 60;
+	//buttonRand.innerText = "playing (click to restart)"
+	buttonRand.innerText = delayInMs;
+	setTimeout("playAudio()", 0);
+	setTimeout("playAudio()", 6000);
+	setTimeout("playAudio()", delayInMs);
+	
+	
+	/*for (let i=0;i<100;i++) {
 		let delay = delIncr * i
 		setTimeout("playAudio()", delay);
-		setTimeout(printIt,delay,delay.toString());
-	}
+		
+	}*/
 }
 
 
