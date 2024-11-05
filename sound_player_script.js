d6cd03357a8936a848059b4ae4d4f154edd2e841
BillFSmith <william.f.smith89@gmail.com>
2024-11-05 11:06:02 +0000

    first image

--- sound_player_script.js
+++ sound_player_script.js
@@ -14,26 +14,48 @@ buttonRand.onclick = playFun;
 buttonDelete.onclick = playFun;
 animToggle.onclick = playFun;
 
+let delIncr = 6000;
+
 function playAudio() {
   playSound.play();
 }
-
-let delIncr = 6000;
 
 function printIt(text){
 	textList.value = text
 }
 
-function printToBox(text, delay) {
-	setTimeout(printIt,delay,text);
+function playFun() {	
+	delIncr = 2000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"2000");
+	
+	delIncr = 8000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"8000");
+	
+	delIncr = 15000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"15000");
+	
+		delIncr = 30000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"30000");
+	
+		delIncr = 60000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"60000");
+	
+		delIncr = 120000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"120000");
+	
+		delIncr = 180000;
+	setTimeout("playAudio()", delIncr);
+	setTimeout(printIt,delIncr,"180000");
 }
 
-function playFun() {
-	let outputList = "playing";
-	let sound = playSound;
-	printToBox(outputList, delIncr);
-	setTimeout("playAudio()", 6000);
-}
+
+
 
 
 
